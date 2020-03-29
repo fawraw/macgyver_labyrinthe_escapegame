c422df21229e6a71f2a354680388a6b21500e2e6
Farid <f.said75@gmail.com>
2020-03-29 18:59:27 +0200

    schema

---
+++ labyrinthe.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+    Module Labyrinthe qui contient la classe Labyrinthe
+"""
+
+import pygame
+
+class Labyrinthe:
+    """Cette classe contient le labyrinthe, controle la strucutre et la positions des objets aléatoires"""
+
+    def __init__(self):
+        """Méthode init"""
+
+    def display
+        """Methode pour l'affichage de l'ecran qui va contenir la structure et la positions"""
