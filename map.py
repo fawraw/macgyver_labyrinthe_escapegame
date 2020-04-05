d8c8d09dadd5965ac5b9c32f56eefce71a7d103b
Farid <f.said75@gmail.com>
2020-04-05 18:07:22 +0200

    remplissage du fichier map.py

---
+++ map.py
@@ -0,0 +1,15 @@
+dccmmmmmmmmccmm
+cmccmcccccccmmm
+ccmcccmmmmmmmcm
+mccmmcccmcccccm
+mmcmmmmcmcmmmmm
+mmcmmmmccccccmm
+mcccccmmmmmmccm
+ccmmmcccmccmcmm
+mcmmmmmcmmcmcmm
+mcmmmcccmmcccmm
+mcccmcmmmmmmmmm
+mmmcmccccmmcccm
+mcmcmcmmccccmcm
+mcccmcmmmcmmmcm
+mmmcmmmmmcmmmca
